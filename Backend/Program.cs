422281e558afbb646f628b6f715716cec4548d61
Geda <gediizeyuu@gmail.com>
2025-01-11 00:06:45 +0300

    adding cors-origins

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -7,6 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 // Add Swagger
 builder.Services.AddEndpointsApiExplorer();
